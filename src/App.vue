da5206be34f09925f323c8f0f61a9466a2dd92f8
Joao Quirino <joao@mitrabi.com>
2021-02-09 20:22:43 -0300

    showPage

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    JOAOs2
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="showPage">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
@@ -11,6 +12,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data: {
+    showPage: true,
+  },
   components: {
     HelloWorld
   }
