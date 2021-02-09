61b3b9928a163ff1aa770a40e041a43f4117ae00
Joao Quirino <joao@mitrabi.com>
2021-02-08 23:44:02 -0300

    dynamic import

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    JOAOs2
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld: () => import('./components/HelloWorld.vue')
   }
 }
 </script>
